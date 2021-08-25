4f188f02ac3567ce5c185a04dad783198edf4e19
Julian <estrotococoa@gmail.com>
2021-08-25 13:13:09 -0300

    changed vite config

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,27 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  let username = '';
+  let INSTA = 'https://instagram.com/'
+  async function handleSearch(){
+    let res = await fetch(INSTA + username + '?__a=1', {
+      method: 'Get'
+    });
+    let data = await res.json();
+    console.log(data);
+  }
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <div class="header">
+    <input type="text" bind:value={username}>
+    <button on:click={handleSearch}>Search</button>
+  </div>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+  .header {
+    padding: 30px 0px;
+    align-items: center;
+    justify-content: center;
+    background: #111;
   }
 </style>
