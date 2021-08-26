2a590702ca8b4577fa69042edb2dcc601a0a1ad1
Julián Pérez Pesce <estrotococoa@gmail.com>
2021-08-26 16:04:57 -0300

    cors fixed, but the api doesnt work

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,21 @@
 <script>
   let username = '';
-  let INSTA = 'https://instagram.com/'
-  async function handleSearch(){
-    let res = await fetch(INSTA + username + '?__a=1', {
-      method: 'Get'
-    });
-    let data = await res.json();
-    console.log(data);
+  let INSTA = "https://instagram.com/";
+  async function searchUser(){
+		let res = await fetch(INSTA+username+"?__a=1",{
+      mode: "no-cors",
+      headers: {"Access-Control-Allow-Origin": "*"},
+			method:"GET"
+		});
+		let data = await res.json();
+		console.log(data);
   }
 </script>
 
 <main>
   <div class="header">
     <input type="text" bind:value={username}>
-    <button on:click={handleSearch}>Search</button>
+    <button on:click={searchUser}>Search</button>
   </div>
 </main>
 
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    cors: {origin: false}
-  },
-  plugins: [svelte()]
+export default defineConfig({  
+  plugins: [svelte()],  
 })
